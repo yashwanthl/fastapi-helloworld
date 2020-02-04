f8ee83749163dd5d4a2f6e64ac8b32484a4c317c
yashwanthl <yashwanthl@mail.usf.edu>
2020-02-04 15:13:34 -0600

    Initial commit

---
+++ app/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI(title="Hello World", version='1.0', description="This is my Hello world application of FastAPI")
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
+
+if __name__ == "__main__":
+    # Use the below line if you want to specify the port and/or host
+    #uvicorn.run(app, port=8000, host="127.0.0.1")
+    uvicorn.run(app)
